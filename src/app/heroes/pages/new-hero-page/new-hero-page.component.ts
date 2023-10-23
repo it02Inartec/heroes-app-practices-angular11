7a4f171a13a5beb1c8ea6fd10efed13bedce3d3c
Jennifer Miranda <jennifer.beuses@gmail.com>
2023-10-22 20:30:05 -0400

    first commit

---
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-hero-page',
+  templateUrl: './new-hero-page.component.html',
+  styles: [
+  ]
+})
+export class NewHeroPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
